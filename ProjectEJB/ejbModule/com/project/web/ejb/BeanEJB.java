37f30665801f6145ff53fa8ef13c9be4fed7e516
PierratSamuel <pierratsamuel@gmail.com>
2012-06-03 19:53:59 +0200

    begin

---
+++ ProjectEJB/ejbModule/com/project/web/ejb/BeanEJB.java
@@ -0,0 +1,21 @@
+package com.project.web.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+
+/**
+ * Session Bean implementation class BeanEJB
+ */
+@Stateless
+@LocalBean
+public class BeanEJB {
+
+    /**
+     * Default constructor. 
+     */
+    public BeanEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
