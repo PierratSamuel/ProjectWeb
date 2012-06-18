24b41dad32510bb36b6b0543fc96313ed764117f
PierratSamuel <pierratsamuel@gmail.com>
2012-06-18 12:38:51 +0200

    renew

---
+++ ProjectEJB/ejbModule/com/project/web/ejb/ProjectBean.java
@@ -0,0 +1,20 @@
+package com.project.web.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ProjectBean
+ */
+@Stateless
+@LocalBean
+public class ProjectBean {
+
+    /**
+     * Default constructor. 
+     */
+    public ProjectBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
